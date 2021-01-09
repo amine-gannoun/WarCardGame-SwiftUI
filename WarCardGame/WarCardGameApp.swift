7d4edbb63096b48aff12844f237a741b3a30f0d3
Amine <drmagannoun@gmail.com>
2021-01-09 10:45:33 +0100

    First commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Amine on 09/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
